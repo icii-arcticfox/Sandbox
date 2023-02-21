6dbf4e61bac694bc18d1ad4dc772c26b49fcf494
Icii <124303586+icii-arcticfox@users.noreply.github.com>
2023-02-21 18:38:44 +0000

    MB Delay ready

--- Verilog/Sandbox.v
+++ Verilog/Sandbox.v
@@ -1,30 +1,33 @@
 module Sandbox(
     /*[Clock]*/ input clk,
-    /*[Reset]*/ input reset,
-
-    input [15:0] input1,
-    
-    output [15:0] output1
+    /*[Reset]*/ input reset
 );
 
 
-//***Using verbose item name
-/*[Previous --signal input1]*/
-reg input1Previous;
+//***Whether we are pipelining data or analyzing the change in a signal over multiple clock cycles,
+//***delaying a signal, or flopping it multiple times, is a common task. That is why we created the
+//***Delay automation. Delay takes a signal name and a delay count, and is attached to a reg.
 
 
-//***Passing in the signal to track, inferred by position
-/*[Previous input1]*/
-reg input1Previous;
+//S***Specifying Item Names
+//***Delay is not a complicated automation, so let's just show it. The two items are --name and --delay.
+//***The signal we are delaying is initial1, and it's delayed value will be set to the attached wire,
+//***named delayedValue1. Delay will use the Previous automation n times to delay the signal.
+
+reg [7:0] initial1;
+/*[Delay --name initial1 --delay 3]*/
+wire [7:0] delayedValue1;
 
 
-//***Infer name, attached register starts with p1_
-//*****ICII'S FAVORITE**
-/*[Previous]*/
-reg p1_input1;
-//***You can build on successive Previous automations by incrementting the suffix to p2_
-/*[Previous]*/
-reg p2_input1;
+
+//S***Using Item Positions
+//***As with most automations, Delay can be used based on the position of the items. The
+//***following is the same example using item position rather than item names, and the delay
+//***amount is changed to 4.
+
+reg [7:0] initial2;
+/*[Delay initial2 4]*/
+wire [7:0] delayedValue2;
 
 
 endmodule
