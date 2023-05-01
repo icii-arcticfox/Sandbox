33442d7bed1ef88f742c3cd377c7c8c9b39f266a
Icii <124303586+icii-arcticfox@users.noreply.github.com>
2023-05-01 07:54:15 +0000

    MB Renamed variables

--- Verilog/Sandbox.v
+++ Verilog/Sandbox.v
@@ -2,9 +2,9 @@ module Sandbox(
     /*[Clock]*/ input clk,
     /*[Reset]*/ input reset,
 
-    reg [15:0] reg1,
-    reg [15:0] reg2,
-    reg [15:0] reg3,
+    input [15:0] var1,
+    input [15:0] var2,
+    input [15:0] var3,
 
     input trigger
 );
@@ -19,9 +19,9 @@ module Sandbox(
 //***sandbox to learn about the /*[<=]*/ automation.
 
 //***Now, let's get to sandboxing! We will demonstrate a reg name sum that will add 3 variables,
-//***reg1, reg2, and reg3, to sum in a parallel fashion. This will occur over 3 clock cycles.
-//***In the first clock cycle, sum will be set to reg1. In the second clock cycle, the value of
-//***reg2 will be added to sum. Last, in the third clock cycle, reg3 will be added to sum.
+//***var1, var2, and var3, to sum in a parallel fashion. This will occur over 3 clock cycles.
+//***In the first clock cycle, sum will be set to var1. In the second clock cycle, the value of
+//***var2 will be added to sum. Last, in the third clock cycle, var3 will be added to sum.
 
 //***Also, since we only want to trigger the signal on the rising edge of writeTrigger, we detect
 //***the rising edge of risingWriteTrigger.
@@ -30,8 +30,8 @@ wire risingTrigger;
 
 reg [15:0] sum;
 /*[sequence --name myMemory  --trigger risingTrigger]*/ begin
-    /*[<= reg1]*/
-    /*[<= sum + reg2]*/
-    /*[<= sum + reg3]*/
+    /*[<= var1]*/
+    /*[<= sum + var2]*/
+    /*[<= sum + var3]*/
 end
 endmodule
